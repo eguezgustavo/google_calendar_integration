57bb92a87475f2562662c0664e5d95abe3e5ebb6
Your Name <you@example.com>
2019-02-07 13:20:22 -0500

    removes not needed comments

--- node_examples/src/index.js
+++ node_examples/src/index.js
@@ -8,17 +8,6 @@ const main = async () => {
     const connection = new GoogleCalendarConnection('eguezgustavo@gmail.com');
     await connection.start();
     const events = await connection.getEvents('2019-02-01T00:00:00Z', '2019-02-06T19:00:00Z');
-    // const auth = await google.auth.getClient({
-    //     scopes: ['https://www.googleapis.com/auth/calendar.events.readonly']
-    // });
-    // const calendar = google.calendar({version: 'v3', auth});
-    // const events = await calendar.events.list({
-    //     calendarId: 'eguezgustavo@gmail.com',
-    //     timeMin: (new Date()).toISOString(),
-    //     maxResults: 10,
-    //     singleEvents: true,
-    //     orderBy: 'startTime'
-    // });
     console.log(events.data.items);
 };
 
